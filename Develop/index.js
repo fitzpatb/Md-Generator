0c9d991310b1fb81cd027d565c25b2848708f9cf
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-03-03 14:57:33 -0700

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+console.log("hello")
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  const generator =
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
